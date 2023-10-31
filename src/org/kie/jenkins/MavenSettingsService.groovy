fa9fe41b812945e0769de5f631e6ff97fae97b41
jstastny-cz <jan.stastny@ibm.com>
2023-10-31 09:39:25 +0100

    check file ls

--- src/org/kie/jenkins/MavenSettingsService.groovy
+++ src/org/kie/jenkins/MavenSettingsService.groovy
@@ -19,27 +19,28 @@ class MavenSettingsService {
         String settingsFile = this.mavenSettingsConfig.settingsXmlPath
         if (this.mavenSettingsConfig.settingsXmlConfigFileId) {
             String settingsFilePath = steps.sh(returnStdout: true, script: 'mktemp --suffix -settings.xml').trim()
+            steps.sh(returnStdout:true, script: "ls -l ${settingsFilePath}")
             steps.configFileProvider([steps.configFile(fileId: this.mavenSettingsConfig.settingsXmlConfigFileId, targetLocation: settingsFilePath, variable: 'MAVEN_SETTINGS_XML')]) {
                 settingsFile = steps.env.'MAVEN_SETTINGS_XML'
             }
+            steps.sh(returnStdout:true, script: "ls -l ${settingsFile}")
         }
-        return settingsFile
-        // if (settingsFile) {
-        //     this.mavenSettingsConfig.dependenciesRepositoriesInSettings.each { MavenSettingsUtils.setRepositoryInSettings(steps, settingsFile, it.key, it.value) }
+        if (settingsFile) {
+            this.mavenSettingsConfig.dependenciesRepositoriesInSettings.each { MavenSettingsUtils.setRepositoryInSettings(steps, settingsFile, it.key, it.value) }
 
-        //     this.mavenSettingsConfig.disabledMirrorRepoInSettings.each {
-        //         MavenSettingsUtils.disableMirrorForRepoInSettings(steps, settingsFile, it)
-        //     }
+            this.mavenSettingsConfig.disabledMirrorRepoInSettings.each {
+                MavenSettingsUtils.disableMirrorForRepoInSettings(steps, settingsFile, it)
+            }
 
-        //     if (this.mavenSettingsConfig.disableSnapshotsInSettings) {
-        //         MavenSettingsUtils.disableSnapshotsInSettings(steps, settingsFile)
-        //     }
+            if (this.mavenSettingsConfig.disableSnapshotsInSettings) {
+                MavenSettingsUtils.disableSnapshotsInSettings(steps, settingsFile)
+            }
 
-        //     this.mavenSettingsConfig.servers.each { MavenSettingsUtils.addServer(steps, settingsFile, it.id, it.username, it.password) }
-        //     return settingsFile
-        // } else {
-        //     return ''
-        // }
+            this.mavenSettingsConfig.servers.each { MavenSettingsUtils.addServer(steps, settingsFile, it.id, it.username, it.password) }
+            return settingsFile
+        } else {
+            return ''
+        }
     }
 
 }
